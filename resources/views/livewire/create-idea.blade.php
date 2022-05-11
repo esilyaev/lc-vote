79854c1a90cbdbd22077f02a7dd58f34c9b585cd
Eugeny Silyaev <esilayev@gmail.com>
2022-05-11 11:22:01 +0300

    tailwind class corted test

--- resources/views/livewire/create-idea.blade.php
+++ resources/views/livewire/create-idea.blade.php
@@ -1,38 +1,36 @@
 <div>
     {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
-    <form wire:submit.prevent="createIdea" action="#" method="post" class="space-y-4 px-4 py-6">
+    <form wire:submit.prevent="createIdea" action="#" method="post" class="px-4 py-6 space-y-4">
         <div>
-            <input wire:model.defer="title" type="text" class="w-full text-sm border-none bg-gray-100 rounded-xl placeholder-gray-900 px-4 py-2" placeholder="Your idea" />
+            <input wire:model.defer="title" type="text" class="w-full px-4 py-2 text-sm placeholder-gray-900 bg-gray-100 border-none rounded-xl" placeholder="Your idea" />
             @error('title')
-            <p class="text-red-500 text-xs">Title must be more than 4 characters</p>
+            <p class="text-xs text-red-500">Title must be more than 4 characters</p>
             @enderror
         </div>
         <div class="">
-            <select wire:model.defer="category" name="" id="" class="w-full text-sm bg-gray-100 rounded-xl border-none px-4 py-2">
+            <select wire:model.defer="category" name="" id="" class="w-full px-4 py-2 text-sm bg-gray-100 border-none rounded-xl">
                 @foreach($categories as $category)
                 <option value="{{ $category->id }}">{{ $category->name }}</option>
                 @endforeach
                 @error('category')
-                <p class="text-red-500 text-xs">Category must be selected</p>
+                <p class="text-xs text-red-500">Category must be selected</p>
                 @enderror
             </select>
         </div>
         <div>
-            <textarea wire:model.defer="description" class="w-full text-sm rounded-xl bg-gray-100 border-none placeholder-gray-900 px-4 py-2" name="idea-description" id="idea-description" cols="30" rows="4" placeholder="Discribe your idea"></textarea>
+            <textarea wire:model.defer="description" class="w-full px-4 py-2 text-sm placeholder-gray-900 bg-gray-100 border-none rounded-xl" name="idea-description" id="idea-description" cols="30" rows="4" placeholder="Discribe your idea"></textarea>
             @error('description')
-            <p class="text-red-500 text-xs">Description must be more than 4 characters</p>
+            <p class="text-xs text-red-500">Description must be more than 4 characters</p>
             @enderror
         </div>
         <div class="flex items-center justify-between space-x-3">
-            <button class="flex items-center bg-gray-100 rounded-xl border border-gray-100 px-6 py-3 w-1/2 h-11 font-semibold
-              hover:border-gray-400 transition duration-150 ease-in">
-                <svg class="w-5 text-gray-900 -rotate-45 transform" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <button class="flex items-center w-1/2 px-6 py-3 font-semibold transition duration-150 ease-in bg-gray-100 border border-gray-100 rounded-xl h-11 hover:border-gray-400">
+                <svg class="w-5 text-gray-900 transform -rotate-45" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
                 </svg>
                 <span class="ml-1">Attach</span>
             </button>
-            <button class="flex items-center text-white bg-blue-500 rounded-xl border border-blue-500 px-6 py-3 w-1/2 h-11 font-semibold
-              hover:bg-blue-700 transition duration-150 ease-in" type="submit">
+            <button class="flex items-center w-1/2 px-6 py-3 font-semibold text-white transition duration-150 ease-in bg-blue-500 border border-blue-500 rounded-xl h-11 hover:bg-blue-700" type="submit">
 
                 <span class="ml-2">Submit</span>
             </button>
--- resources/views/livewire/delete-idea.blade.php
+++ resources/views/livewire/delete-idea.blade.php
@@ -13,10 +13,10 @@
       custom-show-delete-modal
   -->
 
-    <div x-show="isOpen" x-transition.opacity class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+    <div x-show="isOpen" x-transition.opacity class="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-75"></div>
 
-    <div x-show="isOpen" x-transition.duration.300ms class="fixed z-10 inset-0 overflow-y-auto">
-        <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+    <div x-show="isOpen" x-transition.duration.300ms class="fixed inset-0 z-10 overflow-y-auto">
+        <div class="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
             <!-- This element is to trick the browser into centering the modal contents. -->
             <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
 
@@ -30,26 +30,26 @@
           From: "opacity-100 translate-y-0 sm:scale-100"
           To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
       -->
-            <div class="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div class="relative inline-block overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                <div class="px-4 pt-5 pb-4 bg-white sm:p-6 sm:pb-4">
                     <div class="sm:flex sm:items-start">
-                        <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                        <div class="flex items-center justify-center flex-shrink-0 w-12 h-12 mx-auto bg-red-100 rounded-full sm:mx-0 sm:h-10 sm:w-10">
                             <!-- Heroicon name: outline/exclamation -->
-                            <svg class="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                            <svg class="w-6 h-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
                             </svg>
                         </div>
                         <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                            <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">Delete idea</h3>
+                            <h3 class="text-lg font-medium leading-6 text-gray-900" id="modal-title">Delete idea</h3>
                             <div class="mt-2">
                                 <p class="text-sm text-gray-500">Are you sure you want to delete idea? All her data will be permanently removed. This action cannot be undone.</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                    <button wire:click="deleteIdea" type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">Delete</button>
-                    <button @click="isOpen=false" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cancel</button>
+                <div class="px-4 py-3 bg-gray-50 sm:px-6 sm:flex sm:flex-row-reverse">
+                    <button wire:click="deleteIdea" type="button" class="inline-flex justify-center w-full px-4 py-2 text-base font-medium text-white bg-red-600 border border-transparent rounded-md shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">Delete</button>
+                    <button @click="isOpen=false" type="button" class="inline-flex justify-center w-full px-4 py-2 mt-3 text-base font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cancel</button>
                 </div>
             </div>
         </div>
